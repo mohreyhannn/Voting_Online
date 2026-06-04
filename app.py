fcc20434bc8de61cd63fad5eefef822e487c5786
MasRey <mametrehan28@gmail.com>
2026-06-05 03:23:50 +0700

    Merge histories

---
+++ app.py
@@ -0,0 +1,445 @@
+import json
+
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from werkzeug.security import check_password_hash, generate_password_hash
+from database import get_db_connection
+
+
+app = Flask(__name__)
+app.secret_key = "suara-kita-secret-key"
+
+@app.route("/")
+def index():
+
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    cur.execute("SELECT COUNT(*) FROM polls")
+    total_polls = cur.fetchone()["count"]
+
+    cur.execute("SELECT COUNT(*) FROM votes")
+    total_votes = cur.fetchone()["count"]
+
+    cur.close()
+    conn.close()
+
+    return render_template(
+        "index.html",
+        total_polls=total_polls,
+        total_votes=total_votes
+    )
+
+
+@app.route("/polls")
+def polls():
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM polls ORDER BY id DESC;")
+    polls = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return render_template("polls.html", polls=polls)
+
+
+@app.route("/vote/<int:poll_id>")
+def vote(poll_id):
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM polls WHERE id = %s;", (poll_id,))
+    poll = cur.fetchone()
+
+    if poll is None:
+        cur.close()
+        conn.close()
+        return "Polling tidak ditemukan"
+
+    cur.execute("SELECT * FROM options WHERE poll_id = %s;", (poll_id,))
+    options = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return render_template("vote.html", poll=poll, options=options)
+
+
+@app.route("/submit-vote", methods=["POST"])
+def submit_vote():
+
+    poll_id = request.form.get("poll_id")
+    option_id = request.form.get("option_id")
+    voter_ip = request.remote_addr
+
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    # cek apakah sudah voting
+
+    cur.execute("""
+        SELECT votes.id
+        FROM votes
+        JOIN options ON votes.option_id = options.id
+        WHERE options.poll_id = %s
+        AND votes.voter_ip = %s
+    """, (poll_id, voter_ip))
+
+    existing_vote = cur.fetchone()
+
+    if existing_vote:
+        cur.close()
+        conn.close()
+
+        return render_template("already_voted.html")
+
+    cur.execute(
+        """
+        INSERT INTO votes(option_id, voter_ip)
+        VALUES (%s, %s)
+        """,
+        (option_id, voter_ip)
+    )
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+    return redirect(url_for("result", poll_id=poll_id))
+
+@app.route("/admin")
+def admin_dashboard():
+
+    if "admin" not in session:
+        return redirect("/admin/login")
+
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM polls ORDER BY id DESC")
+    polls = cur.fetchall()
+
+    cur.execute("SELECT COUNT(*) FROM polls")
+    total_polls = cur.fetchone()["count"]
+
+    cur.execute("SELECT COUNT(*) FROM options")
+    total_options = cur.fetchone()["count"]
+
+    cur.execute("SELECT COUNT(*) FROM votes")
+    total_votes = cur.fetchone()["count"]
+
+    cur.close()
+    conn.close()
+
+    return render_template(
+        "admin/dashboard.html",
+        polls=polls,
+        total_polls=total_polls,
+        total_options=total_options,
+        total_votes=total_votes
+    )
+
+@app.route("/result/<int:poll_id>")
+def result(poll_id):
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM polls WHERE id = %s;", (poll_id,))
+    poll = cur.fetchone()
+
+    cur.execute("""
+        SELECT 
+            options.id,
+            options.option_text,
+            COUNT(votes.id) AS total_votes
+        FROM options
+        LEFT JOIN votes ON options.id = votes.option_id
+        WHERE options.poll_id = %s
+        GROUP BY options.id, options.option_text
+        ORDER BY options.id;
+    """, (poll_id,))
+
+    results = cur.fetchall()
+    total_votes = sum(item["total_votes"] for item in results)
+
+    for item in results:
+        item["percentage"] = round((item["total_votes"] / total_votes) * 100, 1) if total_votes > 0 else 0
+
+    labels = [item["option_text"] for item in results]
+    data_votes = [item["total_votes"] for item in results]
+    labels_json = json.dumps(labels)
+    data_votes_json = json.dumps(data_votes)
+
+    cur.close()
+    conn.close()
+
+    return render_template(
+    "result.html",
+    poll=poll,
+    results=results,
+    total_votes=total_votes,
+    labels_json=labels_json,
+    data_votes_json=data_votes_json
+)
+
+@app.route("/admin/logout")
+def admin_logout():
+    session.clear()
+    flash("Logout berhasil", "success")
+    return redirect("/admin/login")
+
+@app.route("/admin/login", methods=["GET", "POST"])
+def admin_login():
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+        password = request.form["password"]
+
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        cur.execute(
+            """
+            SELECT *
+            FROM admins
+            WHERE username=%s
+            """,
+            (username,)
+        )
+
+        admin = cur.fetchone()
+
+        cur.close()
+        conn.close()
+
+        if admin and admin["password"] == password:
+
+            session["admin"] = username
+
+            flash(
+                "Login berhasil",
+                "success"
+            )
+
+            return redirect("/admin")
+
+        flash(
+            "Username atau Password salah",
+            "danger"
+        )
+
+    return render_template(
+        "admin/login.html"
+    )
+
+@app.route("/admin/add", methods=["GET", "POST"])
+def add_poll():
+
+    if "admin" not in session:
+        return redirect("/admin/login")
+
+    if request.method == "POST":
+
+        question = request.form["question"]
+
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        cur.execute(
+            """
+            INSERT INTO polls(question)
+            VALUES(%s)
+            """,
+            (question,)
+        )
+
+        conn.commit()
+
+        cur.close()
+        conn.close()
+
+        flash("Polling baru berhasil ditambahkan.", "success")
+        return redirect("/admin")
+
+    return render_template(
+        "admin/add_poll.html"
+    )
+
+@app.route("/admin/edit/<int:id>", methods=["GET", "POST"])
+def edit_poll(id):
+
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    if request.method == "POST":
+
+        question = request.form["question"]
+
+        cur.execute("""
+            UPDATE polls
+            SET question = %s
+            WHERE id = %s
+        """, (question, id))
+
+        conn.commit()
+
+        cur.close()
+        conn.close()
+
+        flash("Pertanyaan polling berhasil diperbarui.", "success")
+        return redirect("/admin")
+
+    cur.execute("""
+        SELECT *
+        FROM polls
+        WHERE id = %s
+    """, (id,))
+
+    poll = cur.fetchone()
+
+    cur.close()
+    conn.close()
+
+    return render_template(
+        "admin/edit_poll.html",
+        poll=poll
+    )
+
+@app.route("/admin/delete/<int:id>")
+def delete_poll(id):
+
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    cur.execute("""
+        DELETE FROM polls
+        WHERE id = %s
+    """, (id,))
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+    flash("Polling berhasil dihapus.", "danger")
+    return redirect("/admin")
+
+@app.route("/admin/options/<int:poll_id>")
+def manage_options(poll_id):
+
+    if "admin" not in session:
+        return redirect("/admin/login")
+
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    cur.execute(
+        "SELECT * FROM polls WHERE id=%s",
+        (poll_id,)
+    )
+
+    poll = cur.fetchone()
+
+    cur.execute(
+        """
+        SELECT *
+        FROM options
+        WHERE poll_id=%s
+        ORDER BY id
+        """,
+        (poll_id,)
+    )
+
+    options = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return render_template(
+        "admin/options.html",
+        poll=poll,
+        options=options
+    )
+
+@app.route(
+    "/admin/options/add/<int:poll_id>",
+    methods=["POST"]
+)
+def add_option(poll_id):
+
+    if "admin" not in session:
+        return redirect("/admin/login")
+
+    option_text = request.form["option_text"]
+
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    cur.execute(
+        """
+        INSERT INTO options(
+            poll_id,
+            option_text
+        )
+        VALUES(%s,%s)
+        """,
+        (poll_id, option_text)
+    )
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+    flash(
+        "Opsi berhasil ditambahkan",
+        "success"
+    )
+
+    return redirect(
+        f"/admin/options/{poll_id}"
+    )
+
+@app.route("/admin/options/delete/<int:id>")
+def delete_option(id):
+
+    if "admin" not in session:
+        return redirect("/admin/login")
+
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    cur.execute(
+        "SELECT poll_id FROM options WHERE id=%s",
+        (id,)
+    )
+
+    option = cur.fetchone()
+
+    if option is None:
+        cur.close()
+        conn.close()
+        flash("Opsi tidak ditemukan", "danger")
+        return redirect("/admin")
+
+    poll_id = option["poll_id"]
+
+    cur.execute(
+        "DELETE FROM options WHERE id=%s",
+        (id,)
+    )
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+    flash("Opsi berhasil dihapus", "danger")
+    return redirect(f"/admin/options/{poll_id}")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ database.py
@@ -0,0 +1,15 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+from psycopg2.extras import RealDictCursor
+
+load_dotenv()
+
+def get_db_connection():
+    return psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        cursor_factory=RealDictCursor
+    )
